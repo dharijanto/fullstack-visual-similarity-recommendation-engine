a3a5858d585f91b1a527bfb161a6ae51cf5aa29e
Denny Harijanto <denny@nusantara-cloud.com>
2020-03-11 23:41:03 +0800

    Working implementation

--- src/app/main-controller.ts
+++ src/app/main-controller.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 
 import * as express from 'express'
+import * as _ from 'lodash'
 import * as Promise from 'bluebird'
 import * as sharp from 'sharp'
 
@@ -28,11 +29,18 @@ class Controller extends BaseController {
 
     // Iterate through all images
     fs.readdir(path.join(AppConfig.IMAGE_PATH, 'lanecrawford'), (err, files) => {
-      const sortedFiles = files.sort()
-      const fileMap = sortedFiles.reduce((acc, value, index) => {
-        acc[value] = index
+      const pageSize = 150
+      const pageLength = files.length / pageSize + (files.length % pageSize > 0 ? 1 : 0)
+
+      const reverseFileMap = {}
+      const fileMap = files.sort().reduce((acc, filename, index) => {
+        acc[filename] = index
+        reverseFileMap[index] = filename
         return acc
       }, {})
+
+      // Randomize the ordering
+      files = _.shuffle(files)
 
       if (err) {
         this.routeGet('*', (req, res, next) => {
@@ -40,8 +48,33 @@ class Controller extends BaseController {
         })
       } else {
         this.routeGet('/', (req, res, next) => {
-          res.locals.sortedFiles = sortedFiles.slice(0,100)
+          res.redirect('/1')
+        })
+        this.routeGet('/:pageId', (req, res, next) => {
+          const pageId = parseInt(req.param('pageId', 1), 10)
+          const start = pageId - 1 * pageSize
+          const end = start + 50 + 1
+          res.locals.sortedFiles = files.slice(start, end)
+          res.locals.pages = _.range(1, pageLength + 1)
           res.render('home')
+        })
+
+        this.routeGet('/visually-similar/:filename', (req, res, next) => {
+          const COSINE_SIM = require(AppConfig.SIMILARITY_MATRIX_PATH)
+          const filename = req.param('filename', null)
+          if (filename) {
+            const similar = COSINE_SIM[fileMap[filename]]
+            if (similar) {
+              const filenames = similar.map((value, index) => {
+                return [value, index]
+              }).sort((a, b) => b[0] - a[0]).slice(1, 10).map(pair => reverseFileMap[pair[1]])
+              res.json({ status: true, data: filenames })
+            } else {
+              res.json({ status: false, errMessage: 'invalid filename@' })
+            }
+          } else {
+            res.json({ status: false, errMessage: 'filename is required!' })
+          }
         })
       }
     })
